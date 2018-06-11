3e2e57371eb9671188545f92efe8a4c4c5eb2dbd
SuperCheese21 <supercheese21@comcast.net>
2018-06-11 16:34:09 -0700

    Added dropdown menu items for each airport

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express');
-const exphb = require('exphb');
+const exphb = require('express-handlebars');
 
-const routes = require('data.json');
+const airports = require('./data.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphb({defaultLayout: 'layoutTemplate'}));
+app.engine('handlebars', exphb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
-app.get('/airports/:n', (req, res) => {
-    var airport = req.params.n;
-    if (routes[airport]) {
-        res.status(200).render('airport', {data: routes[airport]});
-    } else {
-        res.status(404).render('404');
-    }
+app.get('/', (req, res) => {
+    res.status(200).render('airport', {airports: airports});
 });
 
 app.get('*', (req, res) => {
     res.status(404).render('404');
 });
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+})
