145aba234d89de5971babd9521a3f44cdc7ce23f
SuperCheese21 <supercheese21@comcast.net>
2018-06-12 19:38:12 -0700

    Removed search bar and data.json

--- public/index.js
+++ public/index.js
@@ -9,74 +9,6 @@ function initMap() {
 
     var destList = document.getElementsByClassName('dest-list')[0];
 
-    var coords = [{
-        iata: "PDX",
-        coords: {
-            lat: 45.5122,
-            lng: -122.6587
-        }
-    }, {
-        iata: "LAX",
-        coords: {
-            lat: 33.9416,
-            lng: -118.4085
-        }
-    }, {
-        iata: "HNL",
-        coords: {
-            lat: 21.3245,
-            lng: -157.9251
-        }
-    }, {
-        iata: "EWR",
-        coords: {
-            lat: 40.6895,
-            lng: -74.1745
-        }
-    }, {
-        iata: "ORD",
-        coords: {
-            lat: 41.9742,
-            lng: -87.9073
-        }
-    }, {
-        iata: "SEA",
-        coords: {
-            lat: 47.4502,
-            lng: -122.3088
-        }
-    }, {
-        iata: "JFK",
-        coords: {
-            lat: 40.6413,
-            lng: -73.7781
-        }
-    }, {
-        iata: "LAS",
-        coords: {
-            lat: 36.0840,
-            lng: -115.1537
-        }
-    }, {
-        iata: "SFO",
-        coords: {
-            lat: 37.6213,
-            lng: -122.3790
-        }
-    }, {
-        iata: "DEN",
-        coords: {
-            lat: 39.8561,
-            lng: -104.6737
-        }
-    }, {
-        iata: "SAN",
-        coords: {
-            lat: 32.7338,
-            lng: -117.1933
-        }
-    }];
-
     coords.forEach((airport) => {
         var markerOptions = {
             position: airport.coords,
@@ -106,3 +38,71 @@ function initMap() {
         var value = destList.value;
     });
 }
+
+var coords = [{
+    "iata": "PDX",
+    "coords": {
+        "lat": 45.5122,
+        "lng": -122.6587
+    }
+}, {
+    "iata": "LAX",
+    "coords": {
+        "lat": 33.9416,
+        "lng": -118.4085
+    }
+}, {
+    "iata": "HNL",
+    "coords": {
+        "lat": 21.3245,
+        "lng": -157.9251
+    }
+}, {
+    "iata": "EWR",
+    "coords": {
+        "lat": 40.6895,
+        "lng": -74.1745
+    }
+}, {
+    "iata": "ORD",
+    "coords": {
+        "lat": 41.9742,
+        "lng": -87.9073
+    }
+}, {
+    "iata": "SEA",
+    "coords": {
+        "lat": 47.4502,
+        "lng": -122.3088
+    }
+}, {
+    "iata": "JFK",
+    "coords": {
+        "lat": 40.6413,
+        "lng": -73.7781
+    }
+}, {
+    "iata": "LAS",
+    "coords": {
+        "lat": 36.0840,
+        "lng": -115.1537
+    }
+}, {
+    "iata": "SFO",
+    "coords": {
+        "lat": 37.6213,
+        "lng": -122.3790
+    }
+}, {
+    "iata": "DEN",
+    "coords": {
+        "lat": 39.8561,
+        "lng": -104.6737
+    }
+}, {
+    "iata": "SAN",
+    "coords": {
+        "lat": 32.7338,
+        "lng": -117.1933
+    }
+}];
--- server.js
+++ server.js
@@ -1,23 +1,63 @@
 const express = require('express');
 const exphb = require('express-handlebars');
+const MongoClient = require('mongodb').MongoClient;
 
-const airports = require('./data.json');
+const mongoHost = "classmongo.engr.oregonstate.edu";
+const mongoPort = 27017;
+const mongoUser = "cs290_shieldse";
+const mongoPassword = "cs290_shieldse";
+const mongoDBName = "cs290_shieldse";
+
+const mongoURL = 'mongodb://' + mongoUser + ':' + mongoPassword + '@' +
+    mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
+let mongoDB = null;
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.engine('handlebars', exphb({defaultLayout: 'main'}));
+app.engine('handlebars', exphb({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.status(200).render('airport', {data: airports});
+    const routes = mongoDB.collection('routes');
+    const coords = mongoDB.collection('coords');
+    const routesCursor = routes.find({});
+    const coordsCursor = coords.find({});
+
+    routesCursor.toArray((err, routeDocs) => {
+        if (err) {
+            res.status(500).send("Error fetching routes from DB.");
+        } else {
+            console.log(routeDocs);
+            coordsCursor.toArray((err, coordsDocs) => {
+                if (err) {
+                    res.status(500).send("Error fetching coords from DB.");
+                } else {
+                    console.log(coordsDocs);
+                    res.status(200).render('airport', {
+                        routes: routeDocs,
+                        coords: coordsDocs
+                    });
+                }
+            });
+        }
+    });
 });
 
 app.get('*', (req, res) => {
     res.status(404).render('404');
 });
 
-app.listen(port, () => {
-    console.log('Server is running on port ' + port);
-})
+MongoClient.connect(mongoURL, (err, client) => {
+    if (err) {
+        throw err;
+    }
+    mongoDB = client.db(mongoDBName);
+    app.listen(port, () => {
+        console.log("== Server listening on port 3000");
+    });
+});
